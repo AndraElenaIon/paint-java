00b1081aec9cba1a538bb5c3c06326fc98e2a39d
Andra <andraelnn46@gmail.com>
2023-04-21 21:03:16 +0300

    added choicebox for multiple brushes

--- Paint/src/paint/paintfxmlController.java
+++ Paint/src/paint/paintfxmlController.java
@@ -4,7 +4,6 @@
  */
 package paint;
 
-
 import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -20,15 +19,13 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ColorPicker;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.image.PixelReader;
-import javafx.scene.image.PixelWriter;
-import javafx.scene.image.WritableImage;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javax.imageio.ImageIO;
@@ -38,120 +35,148 @@ import javax.imageio.ImageIO;
  * @author Andra
  */
 public class paintfxmlController implements Initializable {
-   
-    
-    
+
     @FXML
     private ColorPicker colorpicker;
-    
+
     @FXML
     private TextField bsize;
-    
+
     boolean toolSelected = false;
-    
+
     @FXML
     private Canvas canvas;
-    
+
     @FXML
     private CheckBox eraser;
-   
-    
+
+    @FXML
+    private ChoiceBox<String> brushTypeChoiceBox;
+
     GraphicsContext brushTool;
-    
-    
+
+    private String currentBrushType = "Circle";
+    private boolean isDrawing = false;
+    private double startX, startY;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         Image eraserIcon = new Image(getClass().getResourceAsStream("eraser.png"));
         Image resizedIcon = PaintUtils.resizeImage(eraserIcon, 20, 20);
-       
+
         ImageView eraserImageView = new ImageView(resizedIcon);
         eraser.setGraphic(eraserImageView);
-        
+
+        brushTypeChoiceBox.getItems().addAll("Circle", "Square", "Line");
+        brushTypeChoiceBox.setValue("Circle");
+
+        brushTypeChoiceBox.setOnAction(e -> {
+            currentBrushType = brushTypeChoiceBox.getValue();
+        });
         brushTool = canvas.getGraphicsContext2D();
         canvas.setOnMouseDragged(e -> {
             double size = Double.parseDouble(bsize.getText());
             double x = e.getX() - size / 2;
             double y = e.getY() - size / 2;
-            
+
             //check if eraser tool is selected
-            if(eraser.isSelected()){
+            if (eraser.isSelected()) {
                 brushTool.clearRect(x, y, size, size);
                 //otherwise proceed with the brush
-            }else if(toolSelected && !bsize.getText().isEmpty()){
+            } else if (toolSelected && !bsize.getText().isEmpty()) {
                 brushTool.setFill(colorpicker.getValue());
-                brushTool.fillRoundRect(x, y, size, size, size, size);
+                switch (currentBrushType) {
+                    case "Circle":
+                        brushTool.fillOval(x, y, size, size);
+                        break;
+                    case "Square":
+                        brushTool.fillRect(x, y, size, size);
+                        break;
+                    case "Line":
+                        if (!isDrawing) {
+                            startX = x;
+                            startY = y;
+                            isDrawing = true;
+                        } else {
+                            brushTool.strokeLine(startX, startY, x, y);
+                            isDrawing = false;
+                        }
+                        break;
+                    default:
+                        brushTool.fillOval(x, y, size, size);
+                        break;
+                }
             }
         });
-        
-    }  
-    
+
+    }
+
     @FXML
-    public void newcanvas(ActionEvent e){
+    public void newcanvas(ActionEvent e) {
         TextField getCanvasWidth = new TextField();
         getCanvasWidth.setPromptText("Width");
         getCanvasWidth.setPrefWidth(150);
         getCanvasWidth.setAlignment(Pos.CENTER);
-        
-         TextField getCanvasHeight = new TextField();
+
+        TextField getCanvasHeight = new TextField();
         getCanvasHeight.setPromptText("Height");
         getCanvasHeight.setPrefWidth(150);
         getCanvasHeight.setAlignment(Pos.CENTER);
-        
+
         Button createButton = new Button();
         createButton.setText("Create Canvas");
-                
+
         VBox vBox = new VBox();
         vBox.setSpacing(5);
         vBox.setAlignment(Pos.CENTER);
         vBox.getChildren().addAll(getCanvasWidth, getCanvasHeight, createButton);
-        
+
         Stage createStage = new Stage();
         AnchorPane root = new AnchorPane();
         root.setPrefWidth(200);
         root.setPrefHeight(200);
         root.getChildren().add(vBox);
-        
+
         Scene CanvasScene = new Scene(root);
         createStage.setTitle("Create Canvas");
         createStage.setScene(CanvasScene);
         createStage.show();
-        
-        
-        createButton.setOnAction(new EventHandler<ActionEvent>(){
-            @Override
-            public void handle(ActionEvent event){
-                double canvasWidthReceived = Double.parseDouble(getCanvasWidth.getText());
-                double canvasHeightReceived = Double.parseDouble(getCanvasHeight.getText());
-                canvas = new Canvas();
-                canvas.setWidth(canvasWidthReceived);
-                canvas.setHeight(canvasHeightReceived);
-                vBox.getChildren().add(canvas);
-                createStage.close();
-            }
+
+        createButton.setOnAction((ActionEvent event) -> {
+            double canvasWidthReceived = Double.parseDouble(getCanvasWidth.getText());
+            double canvasHeightReceived = Double.parseDouble(getCanvasHeight.getText());
+            Canvas newCanvas = new Canvas();
+            newCanvas.setWidth(canvasWidthReceived);
+            newCanvas.setHeight(canvasHeightReceived);
+            Pane parentContainer = (Pane) canvas.getParent();
+            int canvasIndex = parentContainer.getChildren().indexOf(canvas);
+            parentContainer.getChildren().remove(canvas);
+            parentContainer.getChildren().add(canvasIndex, newCanvas);
+
+            canvas = newCanvas;
+            createStage.close();
         });
-        
-        
+
     }
-    
-    @FXML 
-    public void toolselected (ActionEvent e){
+
+    @FXML
+    public void toolselected(ActionEvent e) {
         toolSelected = true;
     }
-   
+
     @FXML
-    public void onSave(){
-        try{
+    public void onSave() {
+        try {
             Image snapshot = canvas.snapshot(null, null);
             ImageIO.write(SwingFXUtils.fromFXImage(snapshot, null), "png", new File("paint.png"));
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println("Failed to save image : " + e);
         }
     }
-    
+
     @FXML
-     public void onExit(){
-         Platform.exit();
+    public void onExit() {
+        Platform.exit();
     }
-    
-    
+
 }
